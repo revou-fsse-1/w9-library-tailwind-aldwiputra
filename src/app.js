cba05a22867d1ec561acc5017dd80b6bd9a6a1c6
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-03-21 11:18:52 +0700

    finish header

--- src/app.js
+++ src/app.js
@@ -12,3 +12,27 @@ async function getBooksData() {
   let json = await response.json();
   return json['books'];
 }
+
+const menuBtn = document.querySelector('#m-menu-button');
+const menu = document.querySelector('#m-menu');
+
+menuBtn.addEventListener('click', event => {
+  const icons = menuBtn.querySelectorAll('svg');
+  const isExpanded = menuBtn.getAttribute('aria-expanded');
+
+  if (isExpanded === 'false') {
+    menuBtn.setAttribute('aria-expanded', true);
+    icons[0].classList.remove('block');
+    icons[0].classList.add('hidden');
+    icons[1].classList.remove('hidden');
+    icons[1].classList.add('block');
+    menu.classList.remove('hidden');
+  } else {
+    menuBtn.setAttribute('aria-expanded', false);
+    icons[0].classList.remove('hidden');
+    icons[0].classList.add('block');
+    icons[1].classList.remove('block');
+    icons[1].classList.add('hidden');
+    menu.classList.add('hidden');
+  }
+});
