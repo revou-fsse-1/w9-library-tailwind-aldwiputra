178331dba5cd213fdc1438c472707c70860696c6
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-03-21 09:53:54 +0700

    add some styling to header

--- src/app.js
+++ src/app.js
@@ -1,24 +1,14 @@
 function getPageFromUrl() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
 
-    let page = urlParams.get('page');
+  let page = urlParams.get('page');
 
-    return page;
+  return page;
 }
 
 async function getBooksData() {
-    let response = await fetch('./data.json');
-    let json = await response.json();
-    return json['books'];
+  let response = await fetch('./data.json');
+  let json = await response.json();
+  return json['books'];
 }
-
-/**
- * To get books data, you should make sure that your function is async function
- * and call `getBooksData` function with await.
- * 
- * e.g.
- * async function yourFunction() {
- *      let books = await getBooksData();
- * }
- */
